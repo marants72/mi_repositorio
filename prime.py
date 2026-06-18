28b1ae4280dd32b1aca3caeb89e31817a02ddbf5
Aslan <mayitosamman@yahoo.com>
2026-06-18 00:52:24 -0600

    commit general

---
+++ prime.py
@@ -0,0 +1,24 @@
+import math
+
+def is_prime(n):
+
+    """funcion para verificar numeros primos"""
+
+    if n <= 1:
+        return False
+    for i in range(2,int(math.sqrt(n) + 1)):
+        if n % i == 0:
+            return False
+    return True
+
+def main():
+
+    """tiene toda la lógica principal"""
+
+    for i in range(100):
+        if is_prime(i):
+            print (i, end=' ')
+    print()
+
+if __name__ == '__main__':
+    main()
